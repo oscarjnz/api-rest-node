e4b833a1555e27d5604e8ca3ba8bf3c8e42574f7
oscarjnz <ojimenez6@est.unibe.edu.do>
2025-09-29 15:23:54 -0400

    Add Not Found Middleware

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Tacking info from login page');
+app.use((req, res, next) => {
+    // res.json({ message: "Hello from Express!" });
+    console.log(req.method)
+    next();
 });
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Taking info from login page' });
+});
+
+import notFound from "./src/middlewares/not-found.js";
+app.use(notFound);
 
 const PORT = 3000;
 
